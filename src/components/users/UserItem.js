511f43e87ae0ff7b6ddc55531e01e0c9570f981c
Soumanpaul <souman1729@gmail.com>
2019-11-02 04:46:12 +0530

    Commiting

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  // const { login, avatar_url, html_url } = props.user;
+
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{
+          // backgroundColor: "red"
+          width: "60px"
+        }}
+      />
+      <h3>{login}</h3>
+
+      <div>
+        <a
+          href={html_url}
+          className="btn btn-dark btn-sm my-1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
